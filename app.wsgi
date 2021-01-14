f26a447e2665e6116240498c89b4b72d2c5bcbb6
Mahesh C. Regmi <regmicmahesh@gmail.com>
2021-01-14 09:05:51 +0000

    fixed all bugs

--- app.wsgi
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import sys
-sys.path.insert(0,"/home/asdf/FlaskApp")
-from app import app as application
--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,14 @@
+#!/usr/bin/python
+import sys
+import site
 
-from app import app
+#this enables python to access installed packages from global python
+site.addsitedir("/home/asdf/env/lib/python3.9/site-packages/")
+
+#this was needed to make sure i can import app i guess
+#i was right
+#if i remove this, i cant import stuffs
+site.addsitedir("/home/asdf/FlaskApp/")
+
+#setting up app which is executable by WSGI Directly.
+from app import app as application
